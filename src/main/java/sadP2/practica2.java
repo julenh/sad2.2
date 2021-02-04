49897c01f4322507113b5cee2f665067a3e6406a
julen <julen@DESKTOP-QS67EV3>
2021-02-04 18:38:14 +0100

    update

--- src/main/java/sadP2/practica2.java
+++ src/main/java/sadP2/practica2.java
@@ -5,7 +5,7 @@ import weka.core.converters.ConverterUtils.DataSource;
 
 import java.io.*;
 
-public class Practica2 {
+public class practica2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -15,5 +15,8 @@ public class Practica2 {
 	public void cargarDatos() {
 		DataSource source = new DataSource("/adult.train.arff");
 		Instances dataset = new source.getDataset();
+		if(dataset.classIndex() == -1) {
+			dataset.setClassIndex(dataset.numAttributes() - 1);
+		}
 	}
 }
