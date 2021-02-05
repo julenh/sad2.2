8a93b386d72fcf18f3e9af8cc0c5e0a2491acf23
julen <julen@DESKTOP-QS67EV3>
2021-02-05 02:12:57 +0100

    update

--- src/main/java/sadP2/practica2.java
+++ src/main/java/sadP2/practica2.java
@@ -20,7 +20,7 @@ import java.io.*;
 
 public class practica2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		clasificacion();
 	}
